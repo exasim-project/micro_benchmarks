ec02f22c5e4a12836dd80c15f01937ec13720502
Hendrik Hetmann <hendrik.hetmann@upstream-cfd.com>
2022-12-12 12:19:17 +0100

    Restructuring of case preparation script, deletion of unused files etc.

--- WindsorBody/case/get_mesh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget https://drive.google.com/uc\?export=download\&id\=18V6h0fjRzTxV8g6ZqSAzqGmopvfyoDRN\&confirm\=t\&uuid=7d2a21bb-0257-4538-bc8b-9d2556b59773\&at\=AHV7M3eU5WdaCruYj48EJRGr3DkK:1668419843949 -O polyMesh.tar.lzma
-tar xvf polyMesh.tar.lzma
-mv polyMesh constant
-rm -rf polyMesh.tar.lzma
