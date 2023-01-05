1dc860c42389a39f2d394caf0d7b434de74a5e0e
Louis Fliessbach <louis.fliessbach@upstream-cfd.com>
2023-01-05 21:05:40 +0100

    Add reference data and update post processing
    - Add reference data from Varney et al. read by post processing scripts
    - Correct Allrun.pre
    - Add description to python scripts
    - Minor changes in python scripts

--- WindsorBody/scripts/python-files/plotScript.py
+++ WindsorBody/scripts/python-files/plotScript.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Description:
+------------
+This is a python for basic post processing of the windsor body case.
 
+Functionality:
+--------------
+The script creates the following plots:
+    - force and moment coefficient time series
+    - Mean and RMS of pressure coefficient along centerline and upper glasshouse line
+
+Script developer : Louis Fliessbach (louis.fliessbach@upstream-cfd.com)
+
+Last updated : 04.01.2023
+"""
 import sys
 import os
 import glob
@@ -17,11 +31,13 @@ if len(sys.argv) > 1:
 else:
     runDir          = '../../run_1'
 
+baseDir = '../../'
+
 
 rawDataDir      = 'postProcessing'
 
 # load information from caseDefinition file in dictionary
-caseDefDict     = io.get_caseDefinition(os.path.join(runDir,'system/include'),'caseDefinition')
+caseDefDict     = io.get_caseDefinition(os.path.join(runDir,'system/include/caseDefinition'))
 
 Aref            = caseDefDict['Aref']
 lref            = caseDefDict['lref']
@@ -41,6 +57,10 @@ if not os.path.exists(imageDirPath):
 figSize         = [7.5,5]    # in cm 
 cm2inch         = lambda cm : cm/2.54
 DPI             = 300
+plt.rcParams['font.size']       = '6'
+plt.rcParams['axes.labelsize']  = '8'
+plt.rcParams['xtick.labelsize'] = '6'
+plt.rcParams['ytick.labelsize'] = '6'
 
 #%% reference point
 # get refrence value for U and p from reference probe
@@ -83,8 +103,9 @@ Cd_ref          = 0.3298
 Cl_ref          = -0.0382
 Cs_ref          = 0.1509
 Cmp_ref         = -0.0259
-Cmy_ref         = 0.0322
-Cmr_ref         = 0.0424
+Cmy_ref         = -0.0322       # sign is changed too match definition of positive yaw angle according to right hand rule
+Cmr_ref         = -0.0424       # sign is changed too match definition of positive roll according to right hand rule
+
 # load transient data from raw OF files
 fileIn          = glob.glob(os.path.join(runDir,rawDataDir,'forceCoeffsAll','*','coefficient.dat'))
 data            = io.mergeOFTimeSeries(fileIn)
@@ -102,8 +123,8 @@ fig             = plt.figure()
 fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
 ax              = fig.add_subplot(1, 1, 1)
 ax.plot(tFull,coeffsFull[:,0],'k-',label='transient')
-ax.plot([-100,100],[coeffsMean[0],coeffsMean[0]],'r--',label='mean')
-ax.plot([-100,100],[Cd_ref,Cd_ref],':',color='grey',label='Exp. Varney et al.')
+ax.plot([-1e6,1e6],[coeffsMean[0],coeffsMean[0]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cd_ref,Cd_ref],':',color='grey',label='Exp. Varney et al.')
 ax.set_xlabel('time in s')
 ax.set_ylabel(r'$C_\mathrm{d}$')
 ax.set_xlim([np.min(tFull),np.max(tFull)])
@@ -117,8 +138,8 @@ fig             = plt.figure()
 fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
 ax              = fig.add_subplot(1, 1, 1)
 ax.plot(tFull,coeffsFull[:,3],'k-',label='transient')
-ax.plot([-100,100],[coeffsMean[3],coeffsMean[3]],'r--',label='mean')
-ax.plot([-100,100],[Cl_ref,Cl_ref],':',color='grey',label='Exp. Varney et al.')
+ax.plot([-1e6,1e6],[coeffsMean[3],coeffsMean[3]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cl_ref,Cl_ref],':',color='grey',label='Exp. Varney et al.')
 ax.set_xlabel('time in s')
 ax.set_ylabel(r'$C_\mathrm{l}$')
 ax.set_xlim([np.min(tFull),np.max(tFull)])
@@ -132,8 +153,8 @@ fig             = plt.figure()
 fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
 ax              = fig.add_subplot(1, 1, 1)
 ax.plot(tFull,coeffsFull[:,9],'k-',label='transient')
-ax.plot([-100,100],[coeffsMean[9],coeffsMean[9]],'r--',label='mean')
-ax.plot([-100,100],[Cs_ref,Cs_ref],':',color='grey',label='Exp. Varney et al.')
+ax.plot([-1e6,1e6],[coeffsMean[9],coeffsMean[9]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cs_ref,Cs_ref],':',color='grey',label='Exp. Varney et al.')
 ax.set_xlabel('time in s')
 ax.set_ylabel(r'$C_\mathrm{s}$')
 ax.set_xlim([np.min(tFull),np.max(tFull)])
@@ -147,8 +168,8 @@ fig             = plt.figure()
 fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
 ax              = fig.add_subplot(1, 1, 1)
 ax.plot(tFull,coeffsFull[:,6],'k-',label='transient')
-ax.plot([-100,100],[coeffsMean[6],coeffsMean[6]],'r--',label='mean')
-ax.plot([-100,100],[Cmp_ref,Cmp_ref],':',color='grey',label='Exp. Varney et al.')
+ax.plot([-1e6,1e6],[coeffsMean[6],coeffsMean[6]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cmp_ref,Cmp_ref],':',color='grey',label='Exp. Varney et al.')
 ax.set_xlabel('time in s')
 ax.set_ylabel(r'$C_\mathrm{m_{Pitch}}$')
 ax.set_xlim([np.min(tFull),np.max(tFull)])
@@ -157,43 +178,43 @@ ax.legend()
 outFile         = os.path.join(imageDirPath,'Cmpitch_t.png')
 fig.savefig(outFile,dpi=DPI, facecolor='w', edgecolor='w', orientation='portrait', bbox_inches='tight', pad_inches=0.1)
 
-# # plot Cm_roll time series
-# fig             = plt.figure()
-# fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
-# ax              = fig.add_subplot(1, 1, 1)
-# ax.plot(tFull,-coeffsFull[:,7],'k-',label='transient')
-# ax.plot([-100,100],[-coeffsMean[7],-coeffsMean[7]],'r--',label='mean')
-# ax.plot([-100,100],[Cmr_ref,Cmr_ref],':',color='grey',label='Exp. Varney et al.')
-# ax.set_xlabel('time in s')
-# ax.set_ylabel(r'$C_\mathrm{m_{Roll}}$')
-# ax.set_xlim([np.min(tFull),np.max(tFull)])
-# ax.set_ylim([0,0.1])
-# ax.legend()
-# outFile         = os.path.join(imageDirPath,'Cmroll_t.png')
-# fig.savefig(outFile,dpi=DPI, facecolor='w', edgecolor='w', orientation='portrait', bbox_inches='tight', pad_inches=0.1)
+# plot Cm_roll time series
+fig             = plt.figure()
+fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
+ax              = fig.add_subplot(1, 1, 1)
+ax.plot(tFull,coeffsFull[:,7],'k-',label='transient')
+ax.plot([-1e6,1e6],[coeffsMean[7],coeffsMean[7]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cmr_ref,Cmr_ref],':',color='grey',label='Exp. Varney et al.')
+ax.set_xlabel('time in s')
+ax.set_ylabel(r'$C_\mathrm{m_{Roll}}$')
+ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_ylim([-0.1,0.1])
+ax.legend()
+outFile         = os.path.join(imageDirPath,'Cmroll_t.png')
+fig.savefig(outFile,dpi=DPI, facecolor='w', edgecolor='w', orientation='portrait', bbox_inches='tight', pad_inches=0.1)
 
-# # plot Cm_yaw time series
-# fig             = plt.figure()
-# fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
-# ax              = fig.add_subplot(1, 1, 1)
-# ax.plot(tFull,-coeffsFull[:,8],'k-',label='transient')
-# ax.plot([-100,100],[-coeffsMean[8],-coeffsMean[8]],'r--',label='mean')
-# ax.plot([-100,100],[Cmy_ref,Cmy_ref],':',color='grey',label='Exp. Varney et al.')
-# ax.set_xlabel('time in s')
-# ax.set_ylabel(r'$C_\mathrm{m_{Yaw}}$')
-# ax.set_xlim([np.min(tFull),np.max(tFull)])
-# ax.set_ylim([0,0.1])
-# ax.legend()
-# outFile         = os.path.join(imageDirPath,'Cmyaw_t.png')
-# fig.savefig(outFile,dpi=DPI, facecolor='w', edgecolor='w', orientation='portrait', bbox_inches='tight', pad_inches=0.1)
+# plot Cm_yaw time series
+fig             = plt.figure()
+fig.set_size_inches(cm2inch(figSize[0]), cm2inch(figSize[1]))
+ax              = fig.add_subplot(1, 1, 1)
+ax.plot(tFull,coeffsFull[:,8],'k-',label='transient')
+ax.plot([-1e6,1e6],[coeffsMean[8],coeffsMean[8]],'r--',label='mean')
+ax.plot([-1e6,1e6],[Cmy_ref,Cmy_ref],':',color='grey',label='Exp. Varney et al.')
+ax.set_xlabel('time in s')
+ax.set_ylabel(r'$C_\mathrm{m_{Yaw}}$')
+ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_ylim([-0.1,0.1])
+ax.legend()
+outFile         = os.path.join(imageDirPath,'Cmyaw_t.png')
+fig.savefig(outFile,dpi=DPI, facecolor='w', edgecolor='w', orientation='portrait', bbox_inches='tight', pad_inches=0.1)
 
 #%% pressure tab probes
 # load reference data  (Max Varney, Giancarlo Pavia, Martin Passmore, Conor Crickmore: https://repository.lboro.ac.uk/articles/dataset/Windsor_Body_Experimental_Aerodynamic_Dataset/13161284)
-inFile          = os.path.join(runDir,'../ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_Pressure_Tapping_Map.csv')
+inFile          = os.path.join(baseDir,'benchmarkData/ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_Pressure_Tapping_Map.csv')
 tabLocations    = io.load_data(inFile,',',Unpack=False)
-inFile          = os.path.join(runDir,'../ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_NW_2_5yaw_Pressures_Averages_Mean.csv')
+inFile          = os.path.join(baseDir,'benchmarkData/ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_NW_2_5yaw_Pressures_Averages_Mean.csv')
 CpMeanRef       = io.load_data(inFile,',',Unpack=False)
-inFile          = os.path.join(runDir,'../ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_NW_2_5yaw_Pressures_Averages_RMS.csv')
+inFile          = os.path.join(baseDir,'benchmarkData/ExpData_LoughboroughUniv_VarneyEtAl/SetB-YawedFlow/Pressure_Mean/SquareBack_NW_2_5yaw_Pressures_Averages_RMS.csv')
 CpRMSRef        = io.load_data(inFile,',',Unpack=False)
 
 CenterlineIdx   = np.where(tabLocations[:,3]==1)[0]
@@ -222,7 +243,7 @@ ax.plot(tabLocations[CenterlineIdx,0],CpMeanRef[CenterlineIdx],'k*',label='Exp. 
 ax.plot(tabLocations[CenterlineIdx,0],CpMean,'r+',label='OF')
 ax.set_xlabel('x in m')
 ax.set_ylabel(r'Mean($C_p$)')
-#ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_xlim([-0.6,0.6])
 #ax.set_ylim([-0.2,0])
 ax.legend()
 outFile         = os.path.join(imageDirPath,'CpMean_Centerline-probes.png')
@@ -235,7 +256,7 @@ ax.plot(tabLocations[CenterlineIdx,0],CpRMSRef[CenterlineIdx],'k*',label='Exp. V
 ax.plot(tabLocations[CenterlineIdx,0],CpRMS,'r+',label='OF')
 ax.set_xlabel('x in m')
 ax.set_ylabel(r'RMS($C_p$)')
-#ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_xlim([-0.6,0.6])
 #ax.set_ylim([-0.2,0])
 ax.legend()
 outFile         = os.path.join(imageDirPath,'CpRMS_Centerline-probes.png')
@@ -262,7 +283,7 @@ ax.plot(tabLocations[GlasshouseIdx,0],CpMeanRef[GlasshouseIdx],'k*',label='Exp. 
 ax.plot(tabLocations[GlasshouseIdx,0],CpMean,'r+',label='OF')
 ax.set_xlabel('x in m')
 ax.set_ylabel(r'Mean($C_p$)')
-#ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_xlim([-0.6,0.6])
 #ax.set_ylim([-0.2,0])
 ax.legend()
 outFile         = os.path.join(imageDirPath,'CpMean_Glasshouse-probes.png')
@@ -275,7 +296,7 @@ ax.plot(tabLocations[GlasshouseIdx,0],CpRMSRef[GlasshouseIdx],'k*',label='Exp. V
 ax.plot(tabLocations[GlasshouseIdx,0],CpRMS,'r+',label='OF')
 ax.set_xlabel('x in m')
 ax.set_ylabel(r'RMS($C_p$)')
-#ax.set_xlim([np.min(tFull),np.max(tFull)])
+ax.set_xlim([-0.6,0.6])
 #ax.set_ylim([-0.2,0])
 ax.legend()
 outFile         = os.path.join(imageDirPath,'CpRMS_Glasshouse-probes.png')
