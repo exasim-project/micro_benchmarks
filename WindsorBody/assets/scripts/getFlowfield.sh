353ee4fa0221454bc22300d0ae32f78251a6cd8b
Louis Fliessbach <louis.fliessbach@upstream-cfd.com>
2023-06-20 21:32:29 +0200

    Change link to initial field

--- WindsorBody/assets/scripts/getFlowfield.sh
+++ WindsorBody/assets/scripts/getFlowfield.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Download and unpack initial flow field
-wget https://upstreamcfdcom-my.sharepoint.com/:f:/g/personal/louis_fliessbach_upstream-cfd_com/EbaA407ZpRFHqKfMx062SeIBAEMrALNvDPCJuE-oETbCbg?download=1 -O windsorBodyDDESInitField.tar.gz
+wget https://upstreamcfdcom-my.sharepoint.com/:u:/g/personal/louis_fliessbach_upstream-cfd_com/EXwR5b2z0JdBtpjo7vDZAbgB0tblTnh80B6aeK7U1fIBKQ?download=1 -O windsorBodyDDESInitField.tar.gz
 tar -xvzf windsorBodyDDESInitField.tar.gz
 rm -rf windsorBodyDDESInitField.tar.gz
