bc8b9c5d8742a557733e7a3f8c1e7d47d1584274
Gregor Olenik <gregor.olenik@web.de>
2023-07-31 08:14:54 +0200

    add preflight check

---
+++ common/preflight.py
@@ -0,0 +1,17 @@
+import os
+from  pprint import pprint 
+from subprocess import check_output
+
+def version_checker(d, cmd_str):
+    cmd = cmd_str.split()
+    d[cmd_str] = check_output(cmd, text=True)
+
+def main():
+    environ = dict(os.environ)
+    version_checker(environ, "mpirun --version")
+    version_checker(environ, "which mpirun")
+    pprint(environ)
+
+
+if __name__ == "__main__":
+    main()
