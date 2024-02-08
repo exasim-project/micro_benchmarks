2b3588809b5f3fc59c2ecd43a2e846285fc4cc6e
Hendrik Hetmann <hendrik.hetmann@upstream-cfd.com>
2024-02-08 11:50:44 +0100

    Updated ImpingingJet for OBR

--- ImpingingJet/basicSetup/Allrun.sh
+++ ImpingingJet/basicSetup/Allrun.sh
@@ -1,6 +1,7 @@
 blockMesh > blockMesh.log
 topoSet > topoSet.log
 createPatch -overwrite > createPatch.log
+cp -r 0.orig 0
 decomposePar > decomposePar.log
 mpirun --use-hwthread-cpus --bind-to core --map-by core -report-bindings pimpleFoam -parallel > pimpleFoam.log
 wait
