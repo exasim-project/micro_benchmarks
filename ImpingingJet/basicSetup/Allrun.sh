ce25c6086cb33e4faf84b1fa1143f137d6318532
Hendrik Hetmann <hendrik.hetmann@upstream-cfd.com>
2024-02-08 11:49:52 +0100

    Updated ImpingingJet for OBR

--- ImpingingJet/basicSetup/Allrun.sh
+++ ImpingingJet/basicSetup/Allrun.sh
@@ -1,7 +1,6 @@
 blockMesh > blockMesh.log
 topoSet > topoSet.log
 createPatch -overwrite > createPatch.log
-sed -i "/topSolidWall/,/\}/ s/        type            patch;/        type            wall;/" ./constant/polyMesh/boundary
 decomposePar > decomposePar.log
 mpirun --use-hwthread-cpus --bind-to core --map-by core -report-bindings pimpleFoam -parallel > pimpleFoam.log
 wait
