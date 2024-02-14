d1602c4edb94acdc6c88bde0147ad3f2f8741e81
Davide Gatti <davide.gatti@kit.edu>
2024-02-14 08:51:53 +0100

    corrected random inlet B.C., switched to LES, removed nonused files

--- impingingJet/basicSetup/Allrun.sh
+++ impingingJet/basicSetup/Allrun.sh
@@ -1,7 +1,7 @@
 blockMesh > blockMesh.log
 topoSet > topoSet.log
 createPatch -overwrite > createPatch.log
-sed -i "/topSolidWall/,/\}/ s/        type            patch;/        type            wall;/" ./constant/polyMesh/boundary
-decomposePar > decomposePar.log
-mpirun --use-hwthread-cpus --bind-to core --map-by core -report-bindings pimpleFoam -parallel > pimpleFoam.log
+cp -r 0.orig 0
+decomposePar -force  > decomposePar.log
+mpirun --np 256 --use-hwthread-cpus --bind-to core --map-by core -report-bindings pimpleFoam -parallel > pimpleFoam.log
 wait
