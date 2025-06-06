931e3566fb397f386e88eb934f50daab8389381a
HendriceH <hendrik.hetmann@upstream-cfd.com>
2025-06-06 15:04:10 +0200

    Added Allrun script for all cases and updated atmFlatTerrain to larger case

--- atmFlatTerrain/assets/scripts/getFlowfieldRANS.sh
+++ atmFlatTerrain/assets/scripts/getFlowfieldRANS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Download and unpack initial flow field
-wget "https://upstreamcfdcom-my.sharepoint.com/:u:/g/personal/hendrik_hetmann_upstream-cfd_com/ETl7BriLmDFIiDB2yGQyMI8BDXf-V-RfYCJdN6mTTUgD8g?download=1" -O atmFlatTerrain_square_restart_RANS_t13000.tar.gz
-tar -xvzf atmFlatTerrain_square_restart_RANS_t13000.tar.gz
-rm -rf atmFlatTerrain_square_restart_RANS_t13000.tar.gz
+wget "https://upstreamcfdcom-my.sharepoint.com/:u:/g/personal/hendrik_hetmann_upstream-cfd_com/EYShwnz5G9pDjTGGyTDmL1oBW6s0Xutt5NRB3fLINSOk9Q?download=1" -O atmFlatTerrain_restart_RANS.tar.gz
+tar -xvzf atmFlatTerrain_restart_RANS.tar.gz
+rm -rf atmFlatTerrain_restart_RANS.tar.gz
